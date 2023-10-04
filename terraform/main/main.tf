1de6f7870c387160eb2248033a9318bb9acf2113
Marcelo Henrique Borgas <marcelo.borgas@edu.fecap.br>
2023-10-04 20:22:41 -0300

    a

---
+++ terraform/main/main.tf
@@ -0,0 +1,15 @@
+
+module "compute" {
+  source = "../modules/compute"
+  # [VARIABLES]
+}
+
+module "network" {
+  source = "../modules/network"
+  # [VARIABLES]
+}
+
+module "database" {
+  source = "../modules/database"
+  # [VARIABLES]
+}
--- terraform/modules/compute/vars.tf
+++ terraform/modules/compute/vars.tf
@@ -1,3 +1,4 @@
+
 # NETWORK VARS DEFAULT VALUES (INPUT IS REQUIRED BECAUSE NO DEFAULT IS DEFINED)
 
 variable "vpc_id" {}
@@ -6,15 +7,6 @@ variable "vpc_sn_pub_az1_id" {}
 variable "vpc_sn_pub_az2_id" {}
 variable "vpc_sg_pub_id" {}
 
-
-# DATABASE VARS DEFAULT VALUES (INPUT IS REQUIRED BECAUSE NO DEFAULT IS DEFINED)
-
-variable "rds_endpoint" {}
-variable "rds_dbuser" {}
-variable "rds_dbpassword" {}
-variable "rds_dbname" {}
-
-
 # COMPUTE VARS DEFAULT VALUES
 
 variable "ec2_lt_name" {
@@ -22,87 +14,30 @@ variable "ec2_lt_name" {
     default = "ec2_lt_name"
 }
 
-variable "ec2_lt_ami" {
-    type    = string
-    default = "ami-02e136e904f3da870"
-}
-
-variable "ec2_lt_instance_type" {
-    type    = string
-    default = "t2.micro"
-}
-
-variable "ec2_lt_ssh_key_name" {
-    type    = string
-    default = "ec2_lt_ssh_key_name"
-}
-
-variable "ec2_lb_name" {
-    type    = string
-    default = "ec2-lb-name"
-}
-
-variable "ec2_lb_tg_name" {
-    type    = string
-    default = "ec2-lb-tg-name"
-}
-
-variable "ec2_lb_tg_protocol" {
-    type    = string
-    default = "HTTP"
-}
-
-variable "ec2_lb_tg_port" {
-    type    = number
-    default = 80
-}
-
-variable "ec2_lb_listener_protocol" {
-    type    = string
-    default = "HTTP"
-}
-
-variable "ec2_lb_listener_port" {
-    type    = number
-    default = 80
-}
-
-variable "ec2_lb_listener_action_type" {
-    type    = string
-    default = "forward"
-}
-
-variable "ec2_asg_name" {
-    type    = string
-    default = "ec2-asg-name"
-}
-
-variable "ec2_asg_desired_capacity" {
-    type    = number
-    default = 8
-}
-
-variable "ec2_asg_min_size" {
-    type    = number
-    default = 4
-}
+# [Other ec2 variables ...]
 
 variable "ec2_asg_max_size" {
     type    = number
     default = 16
 }
 
-# variable "access_key_id" {
-#     type    = string
-#     default = "12345"
-# }
+variable "rds_endpoint" {
+  description = "The endpoint (URL) for the RDS instance."
+  type        = string
+}
 
-# variable "secret_access_key" {
-#     type    = string
-#     default = "12345"
-# }
+variable "rds_dbuser" {
+  description = "The username for the RDS database."
+  type        = string
+}
 
-# variable "session_token" {
-#     type    = string
-#     default = "12345"
-# }
+variable "rds_dbpassword" {
+  description = "The password for the RDS database user."
+  type        = string
+  sensitive   = true
+}
+
+variable "rds_dbname" {
+  description = "The name of the RDS database."
+  type        = string
+}
